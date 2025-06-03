d50613489cefafab134f898e61924f18acffb1b6
OPC Aula Turno 2 Tarde <opcd11@cfp655.local>
2025-06-02 21:50:49 -0300

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,20 @@ import Inicio from './page/Inicio.jsx';
 import Menu from './page/Menu.jsx';
 import Menuinclusivo from './page/Menuinclusivo.jsx';
 import Ubicacion from './page/Ubicacion.jsx';
-import FlowingMenu from './FlowingMenu.tsx'
 import { Link } from 'react-router';
 import { Routes } from 'react-router';
 import { Route } from 'react-router';
-
-const demoItems = [
-  { link: '#', text: 'Mojave', image: 'https://picsum.photos/600/400?random=1' },
-  { link: '#', text: 'Sonoma', image: 'https://picsum.photos/600/400?random=2' },
-  { link: '#', text: 'Monterey', image: 'https://picsum.photos/600/400?random=3' },
-  { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }
-];
-
-
-
+import Rider from './assets/rider.png'
 
 
 export default function App() {
   return (
     
     <>
-    <div style={{ height: '600px', position: 'relative' }}>
-  <FlowingMenu items={demoItems} />
-</div>
-    <nav>
+       <nav>
     <ul style={{display: 'flex', gap: '60px' }}>
-    <li><Link to= 'Inicio'>Inicio</Link></li>
-    <li><Link to= 'Menu'>Menu</Link></li>
+    <li><Link to= '/'>Inicio</Link></li>
+    <li><Link to= '/menu'>Menu</Link></li>
     <li><Link to= 'Menu inclusivo'>Menu Inclusivo</Link></li>
     <li><Link to= 'Ubicacion'>Ubicacion</Link></li>
 
@@ -38,17 +25,11 @@ export default function App() {
   </nav>
    <Routes>
           <Route path="/" element={<Inicio />} />
-          <Route path="/about" element={<Menu />} />
-          <Route path="/contact" element={<Menuinclusivo />} />
-          <Route path="/terapia" element={<Ubicacion />} />
-        
-
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/menuinclusivo" element={<Menuinclusivo />} />
+          <Route path="/ubicacion" element={<Ubicacion />} />
+      
         </Routes>
-
-
-
-
-  
     </>
   )
 }
--- src/page/Inicio.jsx
+++ src/page/Inicio.jsx
@@ -1,6 +1,23 @@
+import FlowingMenu from '../FlowingMenu.tsx'
+
+
+
+const demoItems = [
+  { link: '#', text: 'Inicio', image: 'https://tse4.mm.bing.net/th/id/OIP.s6KPENkVUykN8gMboHxrDwHaHa?rs=1&pid=ImgDetMain' },
+  { link: '#', text: 'Menú', image: 'https://tse1.mm.bing.net/th/id/OIP.4b0hznE1y7OrfC0ghwkjjgHaHa?rs=1&pid=ImgDetMain' },
+  { link: '#', text: 'Menú Inclusivo', image: 'https://tse1.mm.bing.net/th/id/OIP.td3aUV0e8Wy7lQqnFtB5gwHaHa?rs=1&pid=ImgDetMain' },
+  { link: '#', text: 'Ubicación', image: 'https://lh5.googleusercontent.com/p/AF1QipOm-ermO-lOgNoJjk4mdQbbCYqPcbFwHnBZyeVC=w408-h306-k-no' }
+];
+
 export default function Inicio(){
  return (
 <>
+<div className='bg-red-200 h-full'> 
+
+ <div style={{ height: '600px', position: 'relative' }}>
+  <FlowingMenu items={demoItems} />
+</div>
+</div>
     </>
 );
 }
