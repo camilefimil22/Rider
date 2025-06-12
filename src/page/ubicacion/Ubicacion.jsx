c5a11e1a40de4c8ff87decf02916f0ec9cad16c8
OPC Aula Turno 1 Noche <opce02@cfp655.local>
2025-06-12 18:49:33 -0300

    trabajo terminado

--- src/page/ubicacion/Ubicacion.jsx
+++ src/page/ubicacion/Ubicacion.jsx
@@ -36,14 +36,14 @@ export default function Ubicacion(){
         <ImLocation className="text-4xl"/>
 
         </div>
-         <div className=" mt-15 ml-20 hover:text-blue-500 transition-transform duration-300 hover:scale-110 ">
+         <div className=" mt-15 ml-20 hover:text-white transition-transform duration-300 hover:scale-110 ">
         <a className="flex" href="https://www.facebook.com/rider.brewingbar/photos/a.500740104063616/770925757045048/?type=3" target="_blank" rel="noopener noreferrer">
        <BsFacebook className="text-2xl bg-center text-blue-500 "/>
        
         <p className="text-3xl font-mono  ">Rider Brewing Bar</p>
          </a>
         </div>
-        <div className=" mt-15 ml-20 hover:text-gray-500 transition-transform duration-300 hover:scale-110">
+        <div className=" mt-15 ml-20 hover:text-white transition-transform duration-300 hover:scale-110">
           <a className="flex" href="https://www.instagram.com/rider_bar/?hl=es" target="_blank" rel="noopener noreferrer">
          
           <FaInstagram   className="text-2xl bg-center bg-pink-400 text-white"/>
@@ -55,7 +55,7 @@ export default function Ubicacion(){
         
         </div>
    
-        <div className=" mt-15 ml-20 hover:text-gray-500 transition-transform duration-300 hover:scale-110">
+        <div className=" mt-15 ml-20 hover:text-white transition-transform duration-300 hover:scale-110">
           <a className="flex" href="https://www.google.com/search?sca_esv=3689d8f87b454a04&q=rider.brewingbar@gmail.com&nfpr=1&sa=X&ved=2ahUKEwiP5uD2yuqNAxV2rJUCHdNuN3QQvgUoAXoECBUQAg&biw=1272&bih=650&dpr=1" target="_blank" rel="noopener noreferrer">
         <MdOutlineEmail className="text-2xl bg-center  bg-orange-300"/>
         
@@ -63,7 +63,7 @@ export default function Ubicacion(){
           </a>
         </div>
        
-      <div className="flex mt-15 ml-20 hover:text-gray-500 transition-transform duration-300 hover:scale-110">
+      <div className="flex mt-15 ml-20 hover:text-white transition-transform duration-300 hover:scale-110">
           
         <BsWhatsapp className="text-2xl bg-center text-white bg-green-500"/>
           
@@ -71,7 +71,7 @@ export default function Ubicacion(){
         
         </div>
 
-      <div className="flex mt-15 ml-20 hover:text-gray-500 transition-transform duration-300 hover:scale-110">
+      <div className="flex mt-15 ml-20 hover:text-white transition-transform duration-300 hover:scale-110">
           
         <FcAutomotive  className="text-2xl "/>
          
@@ -89,7 +89,7 @@ export default function Ubicacion(){
           <div className="border-black border-2 border-2 transition-transform duration-300 hover:scale-110 bg-cover row-span-2 col-start-4 row-start-3 bg-center bg-[url(https://lh3.googleusercontent.com/gps-cs-s/AC9h4noCHW95RtgFRnvOp8_3tfqYTS0oz1Eh-9gk7kOrBBATYc5zickF9il-FbDA5vzllNaPuEgoo3dLEd1uZGsU6Gkm078-LhKaKb4Zl1Y4oNI5-XBSRXAAaBMhsZ41Hfi6u_g9I0a3=s680-w680-h510-rw)]"></div>
       </div>
     
-  <iframe className="transition-transform duration-300 hover:scale-110 mt-5 flex h-80 w-150 border-black border-2 border-2 " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2921.989596070928!2d-71.3193484!3d-42.9152604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961dada2aacea5e1%3A0x197fcdcecf93b00e!2sRider%20Brewing%20Bar!5e0!3m2!1ses-419!2sar!4v1749511147735!5m2!1ses-419!2sar"></iframe>
+  <iframe className="transition-transform duration-300 hover:scale-110 mt-5 flex h-80 w-140 border-black border-2 border-2 " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2921.989596070928!2d-71.3193484!3d-42.9152604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961dada2aacea5e1%3A0x197fcdcecf93b00e!2sRider%20Brewing%20Bar!5e0!3m2!1ses-419!2sar!4v1749511147735!5m2!1ses-419!2sar"></iframe>
      
       </div>
       
