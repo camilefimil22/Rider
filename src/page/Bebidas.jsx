6180ad9e27ac19e416e22d507de5e7b3eca0ad97
camilefimil22 <camilalefimil@hotmail.com>
2025-06-12 21:52:19 -0300

    fotos

--- src/page/Bebidas.jsx
+++ src/page/Bebidas.jsx
@@ -15,7 +15,7 @@ export default function Bebidas(){
         <div className="grid min-h-[120px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">   
             <img
                 className="object-cover object-center rounded-lg h-10 w-70"
-                src="./src/assets/BEBIDAS.jpg"
+                src="https://filemon.grupobuenrollo.com/files/images/botones/BotonFilemon2.png"
             />
         </div>
         
