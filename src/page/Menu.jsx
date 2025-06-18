837022618adaea45095cc5425d0a265d9d6d120f
Daiana333 <daiana412202@gmail.com>
2025-06-18 18:49:15 -0300

    se modifico la vista

--- src/page/Menu.jsx
+++ src/page/Menu.jsx
@@ -23,6 +23,7 @@ export default function Menu(){
  return (
     <>
         <div className="grid min-h-[120px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">   
+            
             <img
                 className="object-cover object-center rounded-lg h-10 w-70"
                 src="./src/assets/cartel.jpg"
